22b805829c1f5fbb29a88e8fa81d2f84f4a1b0a5
vachan-max <vachanshetty23@gmail.com>
2025-04-20 12:04:40 +0530

    changed some code

--- armstrong.py
+++ armstrong.py
@@ -1,10 +1,15 @@
 n=153
 temp=n
 result=0
+nod = len(str(n))
 while n>0:
     last_digit = n%10
+    result = result+last_digit**nod
+    n=n//10
 
+if result == temp:
+    print("It is an armstrong number",result)
+else:
+    print("It is not an armstrong Number",result)
 
-
-    result
-
+   
